c7c72d09765a81bce7e93de67919879ef68f8c5c
Benjamin Barreto <4544900-benalexb@users.noreply.gitlab.com>
2020-11-08 10:04:59 +0100

    Added graphql schema and tested poc resolver using postman, working as expected

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,19 +1,124 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+// import graphQLTypes from '../../src/graphql/types.gql'
 
 const typeDefs = gql`
-  type Query {
-    users: [User!]!
-  }
+  # USER
   type User {
+    _id: ID!
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+
+  # BOARD
+  type Board {
+    _id: ID!
+    owner: ID!
+    name: String!
+    members: [User!]!
+  }
+
+  input AddBoardInput {
+    owner: ID!
+    name: String!
+    members: [ID!]!
+  }
+
+  input UpdateBoardInput {
+    owner: ID
     name: String
+    members: [ID]
+  }
+
+  # COLUMN
+  type Column {
+    _id: ID!
+    title: String!
+    description: String!
+    position: Int!
+    board: ID!
+  }
+
+  input AddColumnInput {
+    title: String!
+    description: String!
+    position: Int!
+    board: ID!
+  }
+
+  input UpdateColumnInput {
+    title: String
+    description: String
+    position: Int
+    board: ID
+  }
+
+  # STICKIE
+  type Stickie {
+    _id: ID!
+    title: String!
+    description: String!
+    position: Int!
+    column: ID!
+  }
+
+  input AddStickieInput {
+    title: String!
+    description: String!
+    position: Int!
+    column: ID!
+  }
+
+  input UpdateStickieInput {
+    title: String
+    description: String
+    position: Int
+    column: ID
+  }
+
+  # MISC
+  type LoginResponse {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    login(email: String, password: String): LoginResponse
+    getUser(id: ID, email: String): User
+    getBoard(id: ID, memberID: ID): Board
+    getColumn(id: ID, board: ID): Column
+    getStickie(id: ID, column: ID): Stickie
+  }
+
+  type Mutation {
+    addBoard(board: AddBoardInput): Board
+    addColumn(column: AddColumnInput): Column
+    addStickie(stickie: AddStickieInput): Stickie
+
+    updateBoard(id: ID!, boardProps: UpdateBoardInput!): Board
+    updateColumn(id: ID!, columnProps: UpdateColumnInput!): Column
+    updateStickie(id: ID!, stickieProps: UpdateStickieInput!): Stickie
+
+    removeBoard(id: ID!): Board
+    removeColumn(id: ID!): Column
+    removeStickie(id: ID!): Stickie
   }
 `
 
 const resolvers = {
   Query: {
-    users (parent, args, context) {
-      return [{ name: 'Nextjs' }]
-    }
+    login () {},
+    getUser (parent, args, context) {
+      return {
+        _id: '123fasfasdf',
+        firstName: 'dfasd f',
+        lastName: 'asdfasdfa',
+        email: 'dfasdf'
+      }
+    },
+    getBoard () {},
+    getColumn () {},
+    getStickie () {}
   }
 }
 
