7993597df28645350a4be590f803ba2b3c140edd
Benjamin Barreto <4544900-benalexb@users.noreply.gitlab.com>
2020-11-09 12:54:15 +0100

    Removed hardcoded request origin during SSR

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import absoluteUrl from 'next-absolute-url'
 import Layout from '../src/components/Layout'
 import HQ from '../src/components/HQ'
 import { getUser } from '../src/queries'
@@ -12,8 +13,9 @@ const IndexPage = (props) => (
 export async function getServerSideProps ({ req }) {
   try {
     const { cookies: { token } } = req
+    const { origin } = absoluteUrl(req, req.headers.host)
 
-    const response = await fetch('http://localhost:3000/api/graphql', {
+    const response = await fetch(`${origin}/api/graphql`, {
       method: 'POST',
       headers: { 'Content-type': 'application/json', Authorization: token },
       body: JSON.stringify({ query: getUser, variables: { id: '5fa8783597e9c42cb36de632' } })
